47557af8ff0989fcf0fe8c4efda3a2aaadfbadfd
m19031 <m19031@g.metro-cit.ac.jp>
2023-01-10 15:33:28 +0900

    add bikkuri

--- main.go
+++ main.go
@@ -16,7 +16,7 @@ func main() {
 
 func Hello() echo.HandlerFunc {
     return func(c echo.Context) error {
-        return c.String(http.StatusOK, "4409:CI/CD!")
+        return c.String(http.StatusOK, "4409:CI/CD!!")
     }
 }
 
