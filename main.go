fbf9d742d6d0664da1fa943aa5336990e8273361
m19031 <m19031@g.metro-cit.ac.jp>
2023-01-10 14:28:20 +0900

    changed

--- main.go
+++ main.go
@@ -16,12 +16,12 @@ func main() {
 
 func Hello() echo.HandlerFunc {
     return func(c echo.Context) error {
-        return c.String(http.StatusOK, "4409:CD/CI complite!"
+        return c.String(http.StatusOK, "4409:CI/CD!")
     }
 }
 
 func ApiHelloGet() echo.HandlerFunc {
     return func(c echo.Context) error {
-        return c.JSON(http.StatusOK, map[string]interface{}{"studentId": "4409", "message": "CD/CI complete!"})
+        return c.JSON(http.StatusOK, map[string]interface{}{"studentId": "4409", "message": "CI/CD!"})
     }
 }
